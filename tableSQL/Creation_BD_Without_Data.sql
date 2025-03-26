9f1d8b439be3b541d3638e5799525be9a77ddc29
Mari Farid <farid.mari3@etu.univ-lorraine.fr>
2025-03-27 00:29:40 +0100

    ajout d'un champ telephone dans la table utilisateurs

--- tableSQL/Creation_BD_Without_Data.sql
+++ tableSQL/Creation_BD_Without_Data.sql
@@ -174,13 +174,15 @@ CREATE TABLE `utilisateurs` (
   `nom` varchar(255) NOT NULL,
   `prenom` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
+  `telephone` varchar(20) NOT NULL,
   `mot_de_passe` varchar(255) NOT NULL,
   `role` varchar(255) NOT NULL,
   `statut` varchar(255) DEFAULT NULL,
   `nombre_heures` int(11) DEFAULT NULL,
   `reset_token` varchar(255) DEFAULT NULL,
   `reset_token_expiration` datetime DEFAULT NULL,
-  `supprimer` tinyint(1) NOT NULL DEFAULT 0
+  `supprimer` tinyint(1) NOT NULL DEFAULT 0,
+  `responsable` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------

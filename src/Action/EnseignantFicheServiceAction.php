8e0af160b90afcfcd8bedefa5104cdf088ee2584
Mari Farid <farid.mari3@etu.univ-lorraine.fr>
2025-04-02 12:34:19 +0200

    correction front

--- src/Action/EnseignantFicheServiceAction.php
+++ src/Action/EnseignantFicheServiceAction.php
@@ -12,9 +12,10 @@ class EnseignantFicheServiceAction extends Action
         <html lang="fr">
         <head>
             <meta charset="UTF-8">
-            <title>Fiche Prévisionnelle</title>
+            <title>Fiche Prévisionnelle de Service</title>
             <!-- 1) Feuille de style globale qui place #menu à gauche et #main-content à droite -->
             <link rel="stylesheet" href="src/Action/layout.css">
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0/css/bootstrap.min.css">
 
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
